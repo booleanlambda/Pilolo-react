b520c3093bfa71938400447c2f4ad7ecf0278ce8
booleanlambda <61171334+booleanlambda@users.noreply.github.com>
2025-07-20 17:01:11 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  root: './',  // where index.html is
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
