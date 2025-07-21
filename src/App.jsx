c09be91e9986578f3faf3d342294a790180c8a4b
booleanlambda <61171334+booleanlambda@users.noreply.github.com>
2025-07-20 21:31:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Editprofile from './pages/Editprofile';
-import Map from './pages/Map';
-import Login from './pages/Login';
-import Creategame from './pages/Creategame';
-import Wallet from './pages/Wallet';
-import Howto from './pages/Howto';
-import Activities from './pages/Activities';
-import Playersactivities from './pages/Playersactivities';
-import Creatorsactivities from './pages/Creatorsactivities';
-import Logout from './pages/Logout';
-import Profile from './pages/Profile';
+import './App.css';
+
+// Import your page components
+import LoginPage from './pages/Login.jsx';
+import MapPage from './pages/Map.jsx'; // The main game map
+// ... import other pages as you build them
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/editprofile' element={<Editprofile />} />
-        <Route path='/map' element={<Map />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/creategame' element={<Creategame />} />
-        <Route path='/wallet' element={<Wallet />} />
-        <Route path='/howto' element={<Howto />} />
-        <Route path='/activities' element={<Activities />} />
-        <Route path='/playersactivities' element={<Playersactivities />} />
-        <Route path='/creatorsactivities' element={<Creatorsactivities />} />
-        <Route path='/logout' element={<Logout />} />
-        <Route path='/profile' element={<Profile />} />
+        {/* ✅ CHANGE: Set the MapPage as the root/home page */}
+        <Route path="/" element={<MapPage />} />
+        
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Add other routes here as you convert more pages */}
       </Routes>
     </Router>
   );
